647b64ddcc026749999529aa88da8def5de1f143
Frank <frankevinherrera@gmail.com>
2026-06-21 04:47:54 -0400

    configurar ApplicationDbContext y aplicar primera migracion de SQLite

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookfyApi.models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BookfyApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ) : base(options)
+        {
+        }
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<Autor> Autores { get; set; }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using BookfyApi.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source=bookfy.db") );
+
+builder.Services.AddControllers();
+
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
